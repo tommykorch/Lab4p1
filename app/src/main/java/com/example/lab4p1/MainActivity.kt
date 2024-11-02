d032d2fe43a477e7f72afed85bc85bfb653ede5e
tommykorch <tkorchmit@bk.ru>
2024-11-02 14:51:12 +0700

    Part4

--- app/src/main/java/com/example/lab4p1/MainActivity.kt
+++ app/src/main/java/com/example/lab4p1/MainActivity.kt
@@ -2,18 +2,21 @@ package com.example.lab4p1
 import android.widget.Button;
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-
+private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
-    private lateinit var nextButton: Button
+    private lateinit var nextButton: ImageButton
+    private lateinit var prevButton: ImageButton
     private lateinit var questionTextView: TextView
 
     private val questionBank = listOf(
@@ -33,6 +36,7 @@ class MainActivity : AppCompatActivity() {
 
 override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate(Bundle?) called")
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -46,29 +50,31 @@ override fun onCreate(savedInstanceState: Bundle?) {
             findViewById(R.id.false_button)
         nextButton =
         findViewById(R.id.next_button)
+        prevButton = findViewById(R.id.prev_button)
         questionTextView =
         findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener { view: View ->
-            Toast.makeText(
-                this,
-                R.string.correct_toast,
-                Toast.LENGTH_SHORT)
-                .show()
+            checkAnswer(true)
         }
         falseButton.setOnClickListener { view: View ->
-            Toast.makeText(
-                this,
-                R.string.incorrect_toast,
-                Toast.LENGTH_SHORT)
-                .show()
+            checkAnswer(false)
         }
 
         nextButton.setOnClickListener {
         currentIndex = (currentIndex + 1) %
                 questionBank.size
+
         updateQuestion()
         }
+    prevButton.setOnClickListener {
+        currentIndex = if (currentIndex == 0) {
+            questionBank.size - 1 // Переход к последнему вопросу, если текущий первый.
+        } else {
+            (currentIndex - 1) % questionBank.size // Переход к предыдущему вопросу.
+        }
+        updateQuestion()
+    }
         updateQuestion()
         }
         private fun updateQuestion() {
@@ -76,5 +82,43 @@ override fun onCreate(savedInstanceState: Bundle?) {
             questionBank[currentIndex].textResId
             questionTextView.setText(questionTextResId)
     }
-
+    private fun checkAnswer(userAnswer:
+                            Boolean) {
+        val correctAnswer =
+            questionBank[currentIndex].answer
+        val messageResId = if (userAnswer ==
+            correctAnswer) {
+            R.string.correct_toast
+        } else {
+            R.string.incorrect_toast
+        }
+        Toast.makeText(this, messageResId,
+            Toast.LENGTH_SHORT)
+            .show()
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG,
+            "onStart() called")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG,
+            "onResume() called")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG,
+            "onPause() called")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG,
+            "onStop() called")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG,
+            "onDestroy() called")
+    }
 }
