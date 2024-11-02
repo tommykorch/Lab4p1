9195ddbf0a58420596f4ca93a11545685947610a
tommykorch <tkorchmit@bk.ru>
2024-11-02 12:12:39 +0700

    Part2

--- app/src/main/java/com/example/lab4p1/MainActivity.kt
+++ app/src/main/java/com/example/lab4p1/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.lab4p1
+import android.widget.Button;
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +21,24 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        trueButton =
+            findViewById(R.id.true_button)
+        falseButton =
+            findViewById(R.id.false_button)
+        trueButton.setOnClickListener { view: View ->
+            Toast.makeText(
+                this,
+                R.string.correct_toast,
+                Toast.LENGTH_SHORT)
+                .show()
+        }
+        falseButton.setOnClickListener { view: View ->
+            Toast.makeText(
+                this,
+                R.string.incorrect_toast,
+                Toast.LENGTH_SHORT)
+                .show()
+        }
+
     }
 }
